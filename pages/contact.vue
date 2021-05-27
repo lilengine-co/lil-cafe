54dfe33d6f2bd748202fb4d2be9eb438b3f9e0ee
BienVu <bien@sentius.com.au>
2021-05-27 15:38:58 +0700

    LIC: update contact form

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,25 +1,29 @@
 <template>
   <div class="container mx-auto">
-
     <div class="block-space">
       <div class="grid grid-cols-2 place-items-stretch mb-4 lg:mb-8">
         <h1 class="text-2xl lg:text-6xl">Contact</h1>
         <div class="flex items-center justify-end">
-          <img src="~assets/icons/icon-phone.svg?data" class="w-6 lg:w-8 h-auto" />
-          <a href="tel:0460783495" class="text-xl md:text-2xl lg:text-4xl whitespace-nowrap">0460 783 495</a>
+          <img
+            src="~assets/icons/icon-phone.svg?data"
+            class="w-6 lg:w-8 h-auto"
+          />
+          <a
+            href="tel:0460783495"
+            class="text-xl md:text-2xl lg:text-4xl whitespace-nowrap"
+            >0460 783 495</a
+          >
         </div>
       </div>
     </div>
     <div data-paperform-id="dpyl2koj"></div>
-    <script src="https://paperform.co/__embed.min.js"></script>  </div>
+  </div>
 </template>
-
 <script>
 export default {
-
-}
+  head: {
+    script: [{ src: "https://paperform.co/__embed.min.js", body: true }]
+  }
+};
 </script>
-
-<style>
-
-</style>
+<style></style>
