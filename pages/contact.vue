2632df123ee2a779ef1cc97c0ab89d06a766f94e
Liam Hobbs <liam@hobbs.id.au>
2021-05-27 13:14:02 +1000

    new paperform contact form

--- pages/contact.vue
+++ pages/contact.vue
@@ -10,39 +10,7 @@
         </div>
       </div>
     </div>
-    <!-- Begin Mailchimp Signup Form -->
-<div id="mc_embed_signup">
-<p></p>
-<form action="https://lilengine.us7.list-manage.com/subscribe/post?u=4c465bd372a9a63b03f84d3fd&amp;id=20a181c628" class="validate" id="mc-embedded-subscribe-form" method="post" name="mc-embedded-subscribe-form" novalidate="" target="_blank">
-<div id="mc_embed_signup_scroll">
-<div class="grid grid-cols-2 gap-10">
-<div class="mc-field-group block"><input class="mt-0 block w-full px-0.5 border-0 border-b-2 border-gray-200 focus:ring-0 focus:border-black" id="mce-FNAME" name="FNAME" placeholder="First name" type="text" value="" /></div>
-
-<div class="mc-field-group block"><input class="mt-0 block w-full px-0.5 border-0 border-b-2 border-gray-200 focus:ring-0 focus:border-black" id="mce-LNAME" name="LNAME" placeholder="Last name" type="text" value="" /></div>
-
-<div class="mc-field-group block"><select class="block w-full mt-0 px-0.5 border-0 border-b-2 border-gray-200 focus:ring-0 focus:border-black" id="mce-MMERGE6" name="MMERGE6"><option disabled="disabled" selected="selected" value="">What is your question about?</option><option value="First Choice">First Choice</option><option value="Second Choice">Second Choice</option><option value="Third Choice">Third Choice</option> </select></div>
-
-<div class="mc-field-group size1of2 block"><input class="mt-0 block w-full px-0.5 border-0 border-b-2 border-gray-200 focus:ring-0 focus:border-black" id="mce-PHONE" name="PHONE" placeholder="Contact Number" type="text" value="" /></div>
-
-<div class="mc-field-group block"><textarea class="required mt-0 block w-full px-0.5 border-0 border-b-2 border-gray-200 focus:ring-0 focus:border-black" id="mce-MMERGE3" name="MMERGE3" placeholder="Message or question*" rows="6"></textarea></div>
-
-<div class="mc-field-group block"><input class="required email mt-0 block w-full px-0.5 border-0 border-b-2 border-gray-200 focus:ring-0 focus:border-black" id="mce-EMAIL" name="EMAIL" placeholder="Email*" type="email" /></div>
-<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-
-<div aria-hidden="true" style="position: absolute; left: -5000px;"><input name="b_4c465bd372a9a63b03f84d3fd_20a181c628" tabindex="-1" type="text" value="" /></div>
-
-<div class="clear block"><input class="button inline-block bg-yellow-600 text-white px-6 py-2 hover:bg-yellow-800" id="mc-embedded-subscribe" name="subscribe" type="submit" value="Submit" /></div>
-
-<div class="clear block" id="mce-responses">
-<div class="response" id="mce-error-response" style="display:none">&nbsp;</div>
-
-<div class="response" id="mce-success-response" style="display:none">&nbsp;</div>
-</div>
-</div>
-</div>
-</form>
-</div>
-<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[6]='MMERGE6';ftypes[6]='dropdown';fnames[4]='PHONE';ftypes[4]='phone';fnames[0]='EMAIL';ftypes[0]='email';fnames[3]='MMERGE3';ftypes[3]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script><!--End mc_embed_signup-->
+    <div data-paperform-id="dpyl2koj"></div><script>(function() {var script = document.createElement('script'); script.src = "https://paperform.co/__embed.min.js"; document.body.appendChild(script); })()</script>
   </div>
 </template>
 
