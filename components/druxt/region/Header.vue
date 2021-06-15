489c3892a0125067d3ae3253c86263930b94e521
BienVu <bien@sentius.com.au>
2021-06-15 15:47:23 +0700

    LIC: update logo

--- components/druxt/region/Header.vue
+++ components/druxt/region/Header.vue
@@ -3,14 +3,26 @@
   <div class="container mx-auto">
     <nav class="flex items-center md:p-3 flex-wrap">
       <NuxtLink to="/" class="p-2 mr-4 inline-flex items-center">
-        <img class="h-14 md:h-24 w-auto mr-2" src="/logo.gif" alt="LilEngine website">
+        <img
+          class="h-14 md:h-24 w-auto mr-2 logo"
+          src="/logo.svg"
+          alt="LilEngine website"
+        />
       </NuxtLink>
-      <DruxtMenu name="cafe-menu" class="inline-flex lg:flex-grow top-navbar ml-auto relative lg:w-auto" />
+      <DruxtMenu
+        name="cafe-menu"
+        class="inline-flex lg:flex-grow top-navbar ml-auto relative lg:w-auto"
+      />
       <!-- <a href="https://square.site/book/LCAKHXCCVB9CG/plumb-on-tennant-fyshwick-act" target="_blank" class="button p-2 inline-flex items-center" title="Booking a table" style="margin: 0; margin-left: 10px;">
         <font-awesome-icon icon="calendar-alt" style="font-size: 15px" />
         <span>Booking</span>
       </a>-->
-      <NuxtLink to="/booking" class="button p-2 inline-flex items-center" title="Booking a table" style="margin: 0; margin-left: 10px;">
+      <NuxtLink
+        to="/booking"
+        class="button p-2 inline-flex items-center"
+        title="Booking a table"
+        style="margin: 0; margin-left: 10px;"
+      >
         <font-awesome-icon icon="calendar-alt" style="font-size: 15px" />
         <span>Booking</span>
       </NuxtLink>
@@ -18,7 +30,10 @@
   </div>
 </template>
 <style lang="scss">
-  * {
-    box-sizing: border-box;
-  }
+.logo {
+  transform: scale(1.5);
+}
+* {
+  box-sizing: border-box;
+}
 </style>
