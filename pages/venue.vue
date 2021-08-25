45ac5fb0f8c6a0e6f19a82feee29976cf957a6b1
AmandeepKaur01 <88700126+AmandeepKaur01@users.noreply.github.com>
2021-08-25 11:25:35 +1000

    Update venue.vue

--- pages/venue.vue
+++ pages/venue.vue
@@ -25,10 +25,8 @@
           >
           to speak to us about tailoring a package to suit your needs.
         </p>
-        </div>
-        </div>
-        </div>
-        </div>
+        
+        
         <p class="mb-2">
           "Please note we are not accepting dine in bookings during COVID19 lockdown in the ACT". 
         </p>
