de9d15e4a64be91a520c37551de1b7ac2bf9d445
AmandeepKaur01 <88700126+AmandeepKaur01@users.noreply.github.com>
2021-10-14 15:34:47 +1100

    Update Modal.vue

--- components/le/Modal.vue
+++ components/le/Modal.vue
@@ -15,13 +15,13 @@
 
                 <div class="mt-2">
                   <p class="text-white text-lg mb-1">
-                    Our full menu is available Monday through Friday from <span class="font-bold"> 7:00am-3:00pm </span>, Saturday and Sunday <span class="font-bold"> 8:00am-2:00pm </span>.
+                    Our full menu is available Monday through Friday from <span class="font-bold"> 7:00am-3:00pm</span>, Saturday and Sunday <span class="font-bold"> 8:00am-2:00pm</span>.
                   <p>
                   <p class="text-white text-lg">
-                    From Friday <span class="font-bold">15th of October</span> we will be opening for dine-in service, with <span class="font-bold"> Friday Night Tapas </span> from <span class="font-bold"> 3:00pm-6:00pm </span>.
+                    From Friday <span class="font-bold">15th of October</span> we will be opening for dine-in service with <span class="font-bold"> Friday Night Tapas</span> from <span class="font-bold"> 3:00pm-6:00pm</span>.
                   </p>
                   <p class="text-white text-lg mt-2">
-                    Bookings on <a class="text-yellow-600 font-bold"  href="tel:0456567248">0456 567 248</a>.
+                    Book with us on <a class="text-yellow-600 font-bold"  href="tel:0456567248">0456 567 248</a> or via <a class-"text-blue" href="https://www.plumbtennant.com.au/booking">.
                   </p>
                 </div>
               </div>
