840d49d61d55e19866aedee3ac93054ca9711877
AmandeepKaur01 <88700126+AmandeepKaur01@users.noreply.github.com>
2021-10-22 08:56:48 +1100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -32,6 +32,7 @@ export default {
         heading: "All Day Dining",
         link: {
           uri: "/files/Plumb-menu-weekday.pdf"
+          uri: "/files/Friday-Tapas-Menu.pdf"
         }
       },
       {
