a506ee790ce3e348ff4ffcca230c0319d357eec8
feb <dinhhien102@gmail.com>
2022-03-14 01:02:57 +1100

    DEV: Updated the popup content.

--- components/le/Modal.vue
+++ components/le/Modal.vue
@@ -10,27 +10,31 @@
             <div class="sm:flex sm:items-start">
               <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                 <h2 class="text-3xl font-bold leading-6 font-medium text-white mb-3" id="modal-title">
-                  WE ARE OPEN!
+                  <!-- WE ARE OPEN! -->
+                  WE ARE CLOSE TODAY!
+                </h2>
+                <h2 class="text-2xl font-bold leading-6 font-medium text-white mb-3" id="modal-title">
+                  CANBERRA DAY 2022.
                 </h2>
 
                 <div class="mt-2">
                   <p class="text-white text-lg mb-1">
                     Our full menu is available Monday through Friday from <span class="font-bold"> 7:00am-3:00pm</span>, Saturday and Sunday <span class="font-bold"> 8:00am-2:00pm</span>.
-                  <p>
-                  <p class="text-white text-lg">
+                  </p>
+                  <!-- <p class="text-white text-lg">
                     We are open for takeaway and dine-in service.
                   </p>
                   <p class="text-white text-lg mt-2">
                     Book with us on <a class="text-yellow-600 font-bold"  href="tel:0456567248">0456 567 248</a> or via <a class="text-blue-600"  href="https://www.plumbtennant.com.au/booking">Online Ordering</a>.
-                  </p>
+                  </p> -->
                 </div>
               </div>
             </div>
           </div>
           <div class="bg-gray-900 bg-opacity-90 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-            <a @click="close()" href="https://online-ordering.plumbtennant.com.au" target="_blank" class="w-full inline-flex justify-center text-center rounded-md border border-transparent shadow-sm px-4 py-2 button sm:ml-3 sm:w-auto sm:text-sm">
+            <!-- <a @click="close()" href="https://online-ordering.plumbtennant.com.au" target="_blank" class="w-full inline-flex justify-center text-center rounded-md border border-transparent shadow-sm px-4 py-2 button sm:ml-3 sm:w-auto sm:text-sm">
               Order now
-            </a>
+            </a> -->
             <button @click="close()" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 button button--gray sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
               Close
             </button>
