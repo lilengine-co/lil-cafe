ec802873c2c036377aafcb6fb7012b7fa97b3011
AmandeepKaur01 <88700126+AmandeepKaur01@users.noreply.github.com>
2022-03-21 17:17:27 +1100

    Update Modal.vue

--- components/le/Modal.vue
+++ components/le/Modal.vue
@@ -27,7 +27,7 @@
             </div>
           </div>
           <div class="bg-gray-900 bg-opacity-90 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-            <a @click="close()" href="https://online-ordering.plumbtennant.com.au" target="_blank" class="w-full inline-flex justify-center text-center rounded-md border border-transparent shadow-sm px-4 py-2 button sm:ml-3 sm:w-auto sm:text-sm">
+            <a @click="close()" href="https://www.onlineorderingplumbtennant.com.au/" target="_blank" class="w-full inline-flex justify-center text-center rounded-md border border-transparent shadow-sm px-4 py-2 button sm:ml-3 sm:w-auto sm:text-sm">
               Order now
             </a>
             <button @click="close()" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 button button--gray sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
