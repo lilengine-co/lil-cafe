84f8ffa7eb5ba1e23217cf0b2791ea56b7d01f09
AmandeepKaur01 <88700126+AmandeepKaur01@users.noreply.github.com>
2022-04-06 11:45:07 +1000

    Added some text to the site

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="container mx-auto">
+    <div class="text-container"> 
+     <p>Plumb is serving fresh, wholesome, tasty food for breakfast and lunch 7 days. We have a wide variety of gluten free and vegan friendly options. Come and join us for a flavourful ride</p>
+     <p>Explore our full menu or order online to enjoy delicious treats. We are a licensed cafe offers you a bunch of gluten-free and vegan friendly options to serve you the amazing food</p>
+    </div>
     <div class="block-space">
       <ul
         class=" grid gap-4 lg:gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
@@ -57,5 +61,9 @@ export default {
 <style lang="scss">
 * {
   box-sizing: border-box;
+}
+.text-container{
+  color: #003660;
+  text-align: center;
 }
 </style>
