ae84ba6ba8ce9e4b1fb836e129748603868130b8
AmandeepKaur01 <88700126+AmandeepKaur01@users.noreply.github.com>
2022-10-04 09:21:24 +1100

    Update footer hours

--- components/druxt/region/Footer.vue
+++ components/druxt/region/Footer.vue
@@ -28,7 +28,6 @@
                 <div>
                   <div class="font-light">7am - 4pm Weekdays</div>
                   <div class="font-light">8am - 2pm Weekends</div>
-                  <div class="font-light">Closed for Labour Day on 3rd October 2022</div>
                 </div>
               </div>
             </div>
