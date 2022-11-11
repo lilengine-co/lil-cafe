33472978c0ca3f642506fbe1e651a64b303d171b
AmandeepKaur01 <88700126+AmandeepKaur01@users.noreply.github.com>
2022-11-11 19:07:17 +1100

    Update text

--- pages/index.vue
+++ pages/index.vue
@@ -74,7 +74,7 @@ export default {
       },
       {
         cover: "/images/BBQ.jpeg",
-        heading: "BBQ classes commencing from 22nd October",
+        heading: "Book your BBQ Classes now",
         link: {
           uri: "https://www.bbqschool.com.au/pages/bbq-classes-canberra-1"
         }
