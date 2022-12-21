4cebe10e53532e74327143d81de41ce3b377a7bb
AmandeepKaur01 <88700126+AmandeepKaur01@users.noreply.github.com>
2022-12-21 16:34:11 +1100

    Temporary suspension of bookings

--- components/druxt/region/Header.vue
+++ components/druxt/region/Header.vue
@@ -17,7 +17,7 @@
         <font-awesome-icon icon="calendar-alt" style="font-size: 15px" />
         <span>Booking</span>
       </a>-->
-      <NuxtLink
+      <!--<NuxtLink
         to="/booking"
         class="button p-2 inline-flex items-center"
         title="Booking a table"
@@ -25,7 +25,7 @@
       >
         <font-awesome-icon icon="calendar-alt" style="font-size: 15px" />
         <span>Booking</span>
-      </NuxtLink> 
+      </NuxtLink>--> 
     </nav>
   </div>
 </template>
