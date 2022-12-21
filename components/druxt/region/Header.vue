97fa2cff353bcff1cfc1866cf41baf22c2aa14f8
AmandeepKaur01 <88700126+AmandeepKaur01@users.noreply.github.com>
2022-12-21 16:40:38 +1100

    Bookings Suspended Temporarily

--- components/druxt/region/Header.vue
+++ components/druxt/region/Header.vue
@@ -17,6 +17,7 @@
         <font-awesome-icon icon="calendar-alt" style="font-size: 15px" />
         <span>Booking</span>
       </a>-->
+      
       <!--<NuxtLink
         to="/booking"
         class="button p-2 inline-flex items-center"
