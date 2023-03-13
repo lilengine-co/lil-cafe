462bc0e077405db4e0c423ccc5eda8c1c72f3df5
AmandeepKaur01 <88700126+AmandeepKaur01@users.noreply.github.com>
2023-03-14 06:10:57 +1100

    Revert pop-up changes

--- components/le/Modal.vue
+++ components/le/Modal.vue
@@ -10,20 +10,29 @@
             <div class="sm:flex sm:items-start">
               <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                 <h2 class="text-3xl font-bold leading-6 font-medium text-white mb-3" id="modal-title">
-                    WE ARE CLOSED TODAY!
+                    WE ARE OPEN!
                 </h2>
                 <div class="mt-2">
                   <p class="text-white text-lg mb-1">
-                    Sorry for any inconvenience! We reopen tomorrow!
+                    At Plumb, we are very passionate about the food. We like to impress our customers with the love that we serve in every bite. We’ve got you covered for the day ahead!
                   </p>
+                  <p class="text-white text-lg">
+                    We are open for takeaway and dine-in service from Monday through Friday from <span data-v-6831b08e="" class="font-bold"> 7:00am-4:00pm</span>, Saturday and Sunday <span data-v-6831b08e="" class="font-bold"> 8:00am-2:00pm</span>.
+                   </p>
                   <p class="text-white text-lg mt-2">
-                    Call us on <a class="text-yellow-600 font-bold"  href="tel:0456567248">0456 567 248</a>.
+                  Book <a class="text-blue-600"  href="https://www.plumbtennant.com.au/booking">Online</a> or call us on <a class="text-yellow-600 font-bold"  href="tel:0456567248">0456 567 248</a>.
+                  </p>
+                  <p data-v-24c72746="" class="text-white text-lg mt-2">
+                    To book your BBQ School classes, click <a data-v-24c72746="" href="https://www.bbqschool.com.au/pages/bbq-classes-canberra-1" class="text-yellow-600 font-bold">here</a>.
                   </p>
                 </div>
               </div>
             </div>
           </div>
           <div class="bg-gray-900 bg-opacity-90 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+            <a @click="close()" href="https://www.onlineorderingplumbtennant.com.au/" target="_blank" class="w-full inline-flex justify-center text-center rounded-md border border-transparent shadow-sm px-4 py-2 button sm:ml-3 sm:w-auto sm:text-sm">
+              Order now
+            </a>
             <button @click="close()" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 button button--gray sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
               Close
             </button>
